0ed3ee5fa3bf2cdc95a4b241a11480691eb57bab
Владислав Заяц <vlad.zayac.1997@mail.ru>
2021-06-29 00:43:29 +0300

    Добавлена модель и контроллер ресурсов

---
+++ WebResourcesTest/Controllers/ResourceController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebResourcesTest.Controllers
+{
+    public class ResourceController : Controller
+    {
+        // GET: Получить список всех ресурсов
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: Создать ресурс (с возможностью предоставить начальное значение)
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // POST: Перезаписать ресурс
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ResourceController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+    }
+}
---
+++ WebResourcesTest/Models/Resource.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebResourcesTest.Models
+{
+    public class Resource
+    {
+        public int Id_Resource;
+        public string Description;
+    }
+}
